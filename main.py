8c91abc7f6fe0d7249fdef4d0a27027d91808ff3
linux.russia@gmail.com <linux.russia@gmail.com>
2012-11-10 09:36:29 +0400

    add import

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 #coding: utf8
 
+from lib import get_page, search_tags
+
 #TODO: url в параметре (запрос ввода) при запуске программы
 #TODO: Написать ф-ю, находящую все <img> теги
 #TODO: получить список URL из списка ссылок
 #TODO: вывести список в окно (Tkinter)
 #TODO: посчитать количество тегов <br> на странице
 #TODO: найти самый длинный URL из списка ссылок
-
-
 
 def main():
     url = 'https://twitter.com/'
