74df3171bd05375b7c59f6d0949b9bd49ad227d0
linux.russia@gmail.com <linux.russia@gmail.com>
2012-11-10 09:14:52 +0400

    change URL

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def search_tags(page):
     return lst
 
 def main():
-    url = 'https://twitter.com/search?q=linux&src=typd'
+    url = 'https://twitter.com/'
     page = get_page(url)
     lst = search_tags(page)
     print "count of <a> = %s" % len(lst)
